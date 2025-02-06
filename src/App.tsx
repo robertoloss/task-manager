2b08c163e9594bfba31aa7625d59fef7522c9833
Roberto Loss <robertoloss@gmail.com>
2025-02-05 22:47:58 -0500

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import Kanban from "./Kanban";
 import { getColsAndTasks, getProject } from "./models/queries";
 import { useMainStore } from "./zustand/store";
+import { initializeProject } from "./models/init";
+import { db } from "./models/db";
 
 export default function App() {
 	const {
@@ -13,11 +15,18 @@ export default function App() {
     project
 	} = useMainStore()
 
+  console.log("App running")
+
 	useEffect(() => {
 		async function getData() {
-      const projectArr = await getProject('052a8ebd-cbf3-4291-bb8f-0d39f2a03589')
-      if (projectArr.length === 0) return
-      setProject(projectArr[0])
+      initializeProject()
+      //const projectArr = await getProject('052a8ebd-cbf3-4291-bb8f-0d39f2a03589')
+      //if (projectArr.length === 0) return
+      //setProject(projectArr[0])
+
+      const firstProject = (await db.projects.toArray())[0]
+      setProject(firstProject)
+        
 
 			const { tasks, columns } = await getColsAndTasks()
 			setColumns(columns)
